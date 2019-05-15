8ac7c8e4860b5326585e0d54174c17bc2b9c9966
shavit <shavit@users.noreply.github.com>
2019-05-14 21:23:12 -0400

    Test the link functions of the grid

--- lib/mario/grid.ex
+++ lib/mario/grid.ex
@@ -4,33 +4,36 @@ defmodule Mario.Grid do
   defstruct [
     width:        0,
     height:       0,
-    nodes:        [],
+    nodes:        %{},
   ]
 
   @type t :: %__MODULE__{
     width:        integer(),
     height:       integer(),
-    nodes:        list()
+    nodes:        map()
   }
 
   def init(w, h) do
     generate(w, h)
   end
 
+  @doc """
+  Generates width * height grid filled with nodes
+  """
   def generate(width, height) do
     nodes
       = 1..(width * height)
     |> Enum.to_list
-    |> Enum.reduce(%{x: 0, y: 0, nodes: []}, fn _a, acc ->
+    |> Enum.reduce(%{x: 0, y: 0, nodes: %{}}, fn _a, acc ->
       if acc.y < width do
         node = Node.init(acc.x, acc.y)
         acc
-        |> Map.update!(:nodes, &(List.insert_at(&1, 0, node)))
+        |> Map.update!(:nodes, &(Map.put(&1, {node.x, node.y}, node)))
         |> Map.update!(:y, &(&1 + 1))
       else
         node = Node.init(acc.x, acc.y)
         acc
-        |> Map.update!(:nodes, &(List.insert_at(&1, 0, node)))
+        |> Map.update!(:nodes, &(Map.put(&1, {node.x, node.y}, node)))
         |> Map.update!(:x, &(&1 + 1))
         |> Map.update!(:y, fn _x -> 0 end)
       end
@@ -42,5 +45,26 @@ defmodule Mario.Grid do
       height: height,
       nodes: nodes
     }
+  end
+
+  @doc """
+  Creates a bi-directional link between two nodes
+  """
+  def link(grid, n1, n2) do
+    grid
+    |> Map.update!(:nodes, fn x ->
+      x
+      |> Map.update!({n1.x, n1.y}, &(Node.link(&1, n2)))
+      |> Map.update!({n2.x, n2.y}, &(Node.link(&1, n1)))
+    end)
+  end
+
+  def unlink(grid, n1, n2) do
+    grid
+    |> Map.update!(:nodes, fn x ->
+      x
+      |> Map.update!({n1.x, n1.y}, &(Node.unlink(&1, n2)))
+      |> Map.update!({n2.x, n2.y}, &(Node.unlink(&1, n1)))
+    end)
   end
 end
--- test/mario/grid_test.exs
+++ test/mario/grid_test.exs
@@ -12,5 +12,52 @@ defmodule Mario.GridTest do
       assert grid_.height == h
       assert (w * h) == Enum.count(grid_.nodes)
     end
+
+    test "link/3 create a bi-directional link between nodes" do
+      assert grid_ = %Grid{} = Grid.generate(12, 11)
+      n1 = Map.get(grid_.nodes, {9,10})
+      assert not is_nil n1
+      n2 = Map.get(grid_.nodes, {7,4})
+      assert not is_nil n2
+
+      node_count = Enum.count(grid_.nodes)
+
+      grid = Grid.link grid_, n1, n2
+      assert Enum.count(grid.nodes) == node_count
+
+      node2 = Map.get(grid.nodes, {n1.x, n1.y})
+      |> Map.get(:neighbours)
+      |> List.first
+      assert n2.x == node2.x
+      assert n2.y == node2.y
+
+      node1 = Map.get(grid.nodes, {n2.x, n2.y})
+      |> Map.get(:neighbours)
+      |> List.first
+      assert n1.x == node1.x
+      assert n1.y == node1.y
+    end
+
+    test "unlink/3" do
+      assert grid_ = %Grid{} = Grid.generate(41, 44)
+      n1 = Map.get(grid_.nodes, {2, 10})
+      assert not is_nil n1
+      n2 = Map.get(grid_.nodes, {4,12})
+      assert not is_nil n2
+
+      node_count = Enum.count(grid_.nodes)
+      grid = grid_
+      |> Grid.link(n1, n2)
+      |> Grid.link(n1, n2)
+      |> Grid.unlink(n1, n2)
+      assert Enum.count(grid.nodes) == node_count
+
+      assert 0 == Map.get(grid.nodes, {n1.x, n1.y})
+      |> Map.get(:neighbours)
+      |> Enum.count
+      assert 0 == Map.get(grid.nodes, {n2.x, n2.y})
+      |> Map.get(:neighbours)
+      |> Enum.count
+    end
   end
 end
